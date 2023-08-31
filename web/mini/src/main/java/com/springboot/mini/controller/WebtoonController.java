f4059e4abf81f26c47253927955ddcd9c80e3566
Minjung Kang <kmj931208@gmail.com>
2023-08-31 15:14:20 +0900

    search_2

--- web/mini/src/main/java/com/springboot/mini/controller/WebtoonController.java
+++ web/mini/src/main/java/com/springboot/mini/controller/WebtoonController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.springboot.mini.Service.WebtoonService;
 import com.springboot.mini.data.dto.WebtoonDto;
@@ -42,12 +43,5 @@ public class WebtoonController {
         model.addAttribute("webtoonList", webtoonEntity);
         return "webtoons/show";
     }
-
-    // @GetMapping("/webtoons/map/{site}")
-    // public String showBySite(@PathVariable String site, Model model) {
-    //     List<WebtoonDto> webtoonEntityList = webtoonService.searchWebtoonsByEvery(site);
-    //     model.addAttribute("webtoonList", webtoonEntityList);
-    //     return "webtoons/map";
-    //}
 
 }
