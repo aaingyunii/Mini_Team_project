f57d67d5df2b0d88695c91f342a7cc6fc1abdad1
aaingyunii <dlsrbs98@gmail.com>
2023-09-04 18:58:09 +0900

    Update WebtoonServiceImpl.java

--- web/mini/src/main/java/com/springboot/mini/Service/impl/WebtoonServiceImpl.java
+++ web/mini/src/main/java/com/springboot/mini/Service/impl/WebtoonServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
